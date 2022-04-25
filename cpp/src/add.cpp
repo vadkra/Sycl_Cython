b754c07e902a43e696de4c4387f36b341dad4d12
Vadim Kraynyukov <vadimkrai@yahoo.com>
2022-04-25 17:57:33 +0200

    added full file

--- cpp/src/add.cpp
+++ cpp/src/add.cpp
@@ -1,12 +1,31 @@
 #include <CL/sycl.hpp>
 
-int add() {
-	cl::sycl::float4 a = { 1.0, 2.0, 3.0, 4.0 };
-	cl::sycl::float4 b = { 4.0, 3.0, 2.0, 1.0 };
-	cl::sycl::float4 c = { 0.0, 0.0, 0.0, 0.0 };
-	
-	cl::sycl::default_selector device_selector;
-	cl::sycl::queue q{device_selector};
+using namespace cl::sycl;
 
-	return 0;
+
+int main()
+{
+    float a[] = {1.0, 2.0, 3.0, 4.0};
+    float b[] = {1.0, -1.0, 1.0, -1.0};
+    float out[4];
+    unsigned int n = 4;
+    
+    queue q;
+    {
+        buffer<float> aBuf(a, n);
+        buffer<float> bBuf(b, n);
+        buffer<float> outBuf(out, n);
+        q.submit([&](handler& h)
+                 {
+                     auto aAcc = aBuf.get_access<access::mode::read>(h);
+                     auto bAcc = bBuf.get_access<access::mode::read>(h);
+                     auto outAcc = outBuf.get_access<access::mode::write>(h);
+                     h.parallel_for<class sycl_kernel>(range<1>(n), [=](id<1> id)
+                     {
+                         outAcc[id] = aAcc[id] + bAcc[id];
+                     });
+                 });
+    }
+
+    return 0;
 }
